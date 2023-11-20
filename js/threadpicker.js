a6fe8f6cf759b689e665283ee96c5afa8e93d9ba
Ceri Shaw <mrs.cshaw@gmail.com>
2023-11-20 15:24:33 +0000

    Added logic to select the closest thread to the chosen colour

--- js/threadpicker.js
+++ js/threadpicker.js
@@ -1,12 +1,56 @@
+let thread_list;
+
 function test() {
-  console.log("This is working");
   var url = "./data/dmc_to_rgb.json"
   fetch(url)
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((json) => {thread_list = json});
 }
 
 function selectThread(hexCode) {
-  var hexCode = document.getElementsByName('hexCode')[0].value
-  console.log(hexCode);
+  const hexVal = document.getElementsByName('hexCode')[0].value;
+  const chosen_rgb = toRGB(hexVal);
+  let distance_table = [];
+
+  //Calculate distance for all colours in the table
+  distance_table = thread_list.map((thread) => {
+    return calculate_distance(thread, chosen_rgb);
+  });
+
+  const closest = distance_table.reduce((previous_best, current) => {
+    return previous_best.distance > current.distance
+      ? current : previous_best;
+  }, distance_table[0]);
+
+  console.log(thread_list.find((thread) => thread.DMC == closest.id));
+}
+
+function calculate_distance(thread, chosen_rgb){
+  const id = getIdentifier(thread);
+  const distance = getDistance(getRGB(thread), chosen_rgb);
+  return { id : id, distance: distance};
+}
+
+function toRGB(hexVal){
+  const code = hexVal.slice(1);
+  const r = parseInt("0x" + code.slice(0,2));
+  const g = parseInt("0x" + code.slice(2,4));
+  const b = parseInt("0x" + code.slice(4,6));
+  return [r,g,b];
+}
+
+function getDistance(thread_rgb, chosen_rgb){
+  const r_distance = thread_rgb[0] - chosen_rgb[0];
+  const g_distance = thread_rgb[1] - chosen_rgb[1];
+  const b_distance = thread_rgb[2] - chosen_rgb[2];
+
+  return Math.sqrt(r_distance**2 + g_distance**2 + b_distance**2);
+}
+
+function getRGB(thread){
+  return [thread["Red"], thread["Green"], thread["Blue"]];
+}
+
+function getIdentifier(thread){
+  return thread["DMC"];
 }
