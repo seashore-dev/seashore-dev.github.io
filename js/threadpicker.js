ee58685b0926fafeb1364a375fb36f2570f90dfd
Ceri Shaw <mrs.cshaw@gmail.com>
2023-11-20 13:42:09 +0000

    Basic page with colour picker and JSON loaded

--- js/threadpicker.js
+++ js/threadpicker.js
@@ -1,3 +1,12 @@
 function test() {
-  console.log("This is working")
+  console.log("This is working");
+  var url = "./data/dmc_to_rgb.json"
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
+
+function selectThread(hexCode) {
+  var hexCode = document.getElementsByName('hexCode')[0].value
+  console.log(hexCode);
 }
