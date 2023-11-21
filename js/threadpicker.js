ffd8f155a5cf54bbc2e01eda0be6114023a2d492
Ceri Shaw <mrs.cshaw@gmail.com>
2023-11-21 17:20:54 +0000

    Changed to airbnb style guide and fixed up errors

--- js/threadpicker.js
+++ js/threadpicker.js
@@ -1,67 +1,79 @@
-let thread_list;
-var url = "./data/dmc_to_rgb.json";
-fetch(url)
-  .then((response) => response.json())
-  .then((json) => {thread_list = json;});
+let threadList;
+const url = './data/dmc_to_rgb.json';
 
-document.getElementById('find_thread').addEventListener('click', selectThread);
+function toRGB(hexVal) {
+  const code = hexVal.slice(1);
+  const red = code.slice(0, 2);
+  const green = code.slice(2, 4);
+  const blue = code.slice(4, 6);
+  const r = parseInt(`0x${red}`, 16);
+  const g = parseInt(`0x${green}`, 16);
+  const b = parseInt(`0x${blue}`, 16);
+  return [r, g, b];
+}
+
+function getIdentifier(thread) {
+  return thread.DMC;
+}
+
+function getDistance(threadRGB, chosenRGB) {
+  const rDistance = threadRGB[0] - chosenRGB[0];
+  const gDistance = threadRGB[1] - chosenRGB[1];
+  const bDistance = threadRGB[2] - chosenRGB[2];
+
+  return Math.sqrt(rDistance ** 2 + gDistance ** 2 + bDistance ** 2);
+}
+
+function getRGB(thread) {
+  return [thread.Red, thread.Green, thread.Blue];
+}
+
+function calculateDistance(thread, chosenRGB) {
+  const id = getIdentifier(thread);
+  const distance = getDistance(getRGB(thread), chosenRGB);
+  return { id, distance };
+}
+
+function prettyName(thread) {
+  return `DMC: ${thread.DMC} ${thread['Thread Colour Name']}`;
+}
+
+function rgbForCSS(thread) {
+  return `rgb(${thread.Red}, ${thread.Green}, ${thread.Blue})`;
+}
 
 function selectThread() {
-  const hexVal = document.getElementsByName('hexCode')[0].value;
-  const thread_colour = document.getElementById('colour_block');
-  const thread_name = document.getElementById('colour_name');
-  const chosen_rgb = toRGB(hexVal);
-  let distance_table = [];
+  const hexVal = document.getElementById('hex_code').value;
+  const threadColour = document.getElementById('colour_block');
+  const threadName = document.getElementById('colour_name');
+  const chosenRGB = toRGB(hexVal);
+  let distanceTable = [];
 
-  //Calculate distance for all colours in the table
-  distance_table = thread_list.map((thread) => {
-    return calculate_distance(thread, chosen_rgb);
-  });
+  // Calculate distance for all colours in the table
+  distanceTable = threadList.map((thread) => calculateDistance(thread, chosenRGB));
 
-  const closest = distance_table.reduce((previous_best, current) => {
-    return previous_best.distance > current.distance
-      ? current : previous_best;
-  }, distance_table[0]);
+  const closest = distanceTable.reduce(
+    (previousBest, current) => {
+      if (previousBest.distance > current.distance) {
+        return current;
+      }
+      return previousBest;
+    },
+    distanceTable[0],
+  );
 
-  const closest_thread = thread_list.find((thread) => thread.DMC == closest.id);
-  thread_name.innerHTML = prettyName(closest_thread);
-  thread_colour.style.backgroundColor = rgb_for_css(closest_thread);
+  const closestThread = threadList.find((thread) => thread.DMC === closest.id);
+  threadName.innerHTML = prettyName(closestThread);
+  threadColour.style.backgroundColor = rgbForCSS(closestThread);
 }
 
-function calculate_distance(thread, chosen_rgb){
-  const id = getIdentifier(thread);
-  const distance = getDistance(getRGB(thread), chosen_rgb);
-  return { id : id, distance: distance};
-}
+fetch(url)
+  .then((response) => response.json())
+  .then((json) => { threadList = json; });
 
-function toRGB(hexVal){
-  const code = hexVal.slice(1);
-  const r = parseInt("0x" + code.slice(0,2));
-  const g = parseInt("0x" + code.slice(2,4));
-  const b = parseInt("0x" + code.slice(4,6));
-  return [r,g,b];
-}
-
-function getDistance(thread_rgb, chosen_rgb){
-  const r_distance = thread_rgb[0] - chosen_rgb[0];
-  const g_distance = thread_rgb[1] - chosen_rgb[1];
-  const b_distance = thread_rgb[2] - chosen_rgb[2];
-
-  return Math.sqrt(r_distance**2 + g_distance**2 + b_distance**2);
-}
-
-function getRGB(thread){
-  return [thread["Red"], thread["Green"], thread["Blue"]];
-}
-
-function getIdentifier(thread){
-  return thread["DMC"];
-}
-
-function prettyName(thread){
-  return "DMC:" + thread["DMC"] + " " + thread["Thread Colour Name"];
-}
-
-function rgb_for_css(thread){
-  return "rgb(" + thread["Red"] + "," + thread["Green"] + "," + thread["Blue"] + ")";
-}
+window.addEventListener('DOMContentLoaded', () => {
+  const el = document.getElementById('find_thread');
+  if (el) {
+    el.addEventListener('click', selectThread, false);
+  }
+});
